b85e10bcf991e0e8c571df4a4e98c87b88a0a853
Vikhu22G <vikhyatjainy@gmail.com>
2026-02-12 23:04:30 +0530

    Clauses

--- Basics/2_Basics.sql
+++ Basics/2_Basics.sql
@@ -33,3 +33,63 @@ SELECT
 FROM 
     Customers;
 -- In this example, COALESCE is used to replace NULL phone numbers with a default message.
+
+
+-- Cross Join to get Cartesian Product of two tables
+-- Example Database: Products and Categories
+SELECT 
+    p.ProductName,
+    c.CategoryName
+FROM 
+    Products p
+CROSS JOIN 
+    Categories c;
+-- This query returns all possible combinations of products and categories.
+-- Using EXISTS to check for the existence of related records
+-- Example Database: Customers and Orders
+SELECT 
+    c.CustomerID,
+    c.CustomerName
+FROM 
+    Customers c
+WHERE 
+    EXISTS (SELECT 1 FROM Orders o WHERE o.CustomerID = c.CustomerID);
+-- This query retrieves customers who have placed at least one order.
+
+-- Full outer join to get all records from both tables
+-- We use a UNION of LEFT JOIN and RIGHT JOIN to achieve this since FULL OUTER JOIN is not supported in all databases
+-- Example Database: Employees and Departments
+SELECT 
+    e.EmployeeID,
+    e.FirstName,            
+    e.LastName,
+    d.DepartmentName
+FROM 
+    Employees e
+LEFT JOIN 
+    Departments d ON e.DepartmentID = d.DepartmentID
+UNION
+SELECT 
+    e.EmployeeID,
+    e.FirstName,        
+    e.LastName,
+    d.DepartmentName
+FROM 
+    Employees e
+RIGHT JOIN 
+    Departments d ON e.DepartmentID = d.DepartmentID;
+-- This query retrieves all employees and their department names, including those without a department and all departments
+
+Clause - Using Clause to filter results based on conditions
+-- Example Database: Orders and Customers
+SELECT 
+    o.OrderID,
+    c.CustomerName
+FROM    
+    Orders o
+JOIN       Customers c USING (CustomerID)
+WHERE 
+    o.OrderDate > '2024-01-01';
+-- This query retrieves orders placed after January 1, 2024, along with the customer names, using the USING clause to join on CustomerID.
+-- It wont work if the column names are different in both tables. In that case we need to use ON clause instead of USING clause.
+
