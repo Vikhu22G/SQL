7f19bca9e7471935a62f2f73be4e53b311feb9a6
Vikhu22G <vikhyatjainy@gmail.com>
2026-04-05 23:39:18 +0530

    Subqueries

--- Basics/7_Basics.sql
+++ Basics/7_Basics.sql
@@ -2,3 +2,18 @@
 Set Autocommit = 0 -- Setting Off/Disabling it
 Commit -- Need to Manually Commit, SAVE changes to database.
 Rollback -- To go to previous commits if commit not done, Revert Chnages to last safe point.
+
+-- Drop Primary Key
+Alter table <Table> drop Primary Key
+
+-- Drop a Unique Constraint
+Alter table <Table> drop Index <Index>
+
+-- Auto Increment Starts with 1, we can change the starting value.
+Alter Table <Table> auto_increment=1000 -- To start with 1000
+
+-- Foreign Keys
+Constraint <Name> Foreign Key(<COL>) References <Table>(<Col>), On delete Cascade, On delete Set Null, On delete Update, On Delete Restrict (Prevent deletion of the Parent if Child Exists)
+
+-- To view indexes and constraints
+Show index form <Table>
---
+++ Basics/8_Basics.sql
@@ -0,0 +1,22 @@
+-- Joins
+-- To combine rows form 2 or more tables.
+
+1) Inner Join
+2) Left Join
+3) Right Join
+
+-- Union and Union All
+-- union - Combine results of 2 or more tables (Removes duplicates by default)
+-- union all - Does not remove duplicates
+
+-- Self Join - Join with same table - Useful when rows in same table are related to each other.
+
+-- View - Snapshot of a Table at a Instance
+
+-- Inedxes - To search faster in tables.
+-- Cardinality and Collation
+-- Create index <Name> on <Table>(<Column>) -- Single Index
+-- Create index <Name> on <Table>(<Column>,<Column2>) -- Multi Index
+-- Drop Index <Index_name> on <Table>
+
+-- Subqueries - Query inside a Query
