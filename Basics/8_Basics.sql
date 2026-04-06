8b0b9af446b8b096f0d1cf321179cd985cf6b28d
Vikhu22G <vikhyatjainy@gmail.com>
2026-04-06 23:16:55 +0530

    Recursion in Sql

--- Basics/8_Basics.sql
+++ Basics/8_Basics.sql
@@ -20,3 +20,39 @@
 -- Drop Index <Index_name> on <Table>
 
 -- Subqueries - Query inside a Query
+
+-- Group by and Having
+-- Stored Procedures - SAVED SQL block that cab be executed later
+Delimiter $$ -- To change for ; occurances in procedure
+Create Procedure Select_users()
+Begin
+Select * from Users;
+End $$
+Delimiter ;
+
+Call Select_users()
+
+-- Triggers
+-- A special kind of stored program that is executed automatically when a specific even occurs in a table;
+Create trigger <name>
+After Insert on <table_name>
+for each row
+Begin
+<Statement>
+End
+
+-- Drop trigger if exists <Trigger_name>
+-- Summary
+-- Before/After
+-- Insert/Update/Delete
+-- New.Column -- Refers to new row
+-- Old.Column -- Refers to old row
+-- For Each Row
+
+-- Wildcards -> % and _
+-- Offset - Used with Limit (To skip Records) - Used in Pagination
+-- Distinct Keyword - Returns unique values only
+-- Truncate table <table_name> - Schema is there but data drops, empties the table
+-- Change - rename and change datatype -> Alter table <table> change column <name> <new_name> <dt>
+-- Modify - Only change datatypes
+
